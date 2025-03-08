b0e039a3f8e7f9f28d810c4ed30ad22b3eca6a78
Mihar Kathiriya <100096596+Miharr@users.noreply.github.com>
2025-03-08 17:05:55 -0600

    added includes

--- Stationary_Management-main/Stationary_Management-main/index.php
+++ Stationary_Management-main/Stationary_Management-main/index.php
@@ -1,5 +1,8 @@
 <?php
-// index.php - Initial commit with basic structure
+// index.php - Added includes and session start
+include('./includes/connect.php');
+include('./functions/common_functions.php');
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
