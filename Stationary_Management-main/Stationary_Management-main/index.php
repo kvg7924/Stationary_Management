ad65d5f785208a657734f222aeaa3323f1f002a7
Mihar Kathiriya <100096596+Miharr@users.noreply.github.com>
2025-03-08 17:14:13 -0600

    added file location

--- Stationary_Management-main/Stationary_Management-main/index.php
+++ Stationary_Management-main/Stationary_Management-main/index.php
@@ -1,5 +1,4 @@
 <?php
-// index.php - Added includes and session start
 include('./includes/connect.php');
 include('./functions/common_functions.php');
 session_start();
@@ -7,8 +6,13 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Ecommerce Home Page</title>
+    <meta charset="UTF-8">
+    <!-- Added meta viewport for responsive design -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce Home Page</title>
+    <!-- Link to Bootstrap and custom CSS -->
+    <link rel="stylesheet" href="./assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="./assets/css/main.css" />
 </head>
 <body>
   <!-- Future content goes here -->
