3f6514da4dfcba283c7b019e711f157bcf4f8259
kvg7924 <kvg79924@gmail.com>
2025-03-09 12:02:24 -0600

    update admin registration
    untill head part

---
+++ Stationary_Management-main/Stationary_Management-main/admin_registration.php
@@ -0,0 +1,15 @@
+<?php
+include('../includes/connect.php');
+include('../functions/common_functions.php');
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce Admin Registration</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="../assets/css/main.css" />
+</head>
