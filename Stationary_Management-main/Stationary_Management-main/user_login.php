1ae28390c288696e0aa46d1c16be5f138440b131
Nups1910 <nupurpatel1910.np@gmail.com>
2025-03-15 18:42:55 -0600

    Added login authentication with session handling and cart check

--- Stationary_Management-main/Stationary_Management-main/user_login.php
+++ Stationary_Management-main/Stationary_Management-main/user_login.php
@@ -48,3 +48,38 @@ include('../functions/common_functions.php');
 </body>
 
 </html>
+<?php
+if (isset($_POST['user_login'])) {
+    $user_username = $_POST['user_username'];
+    $user_password = $_POST['user_password'];
+    $select_query = "SELECT * FROM `user_table` WHERE username='$user_username'";
+    $select_result = mysqli_query($con, $select_query);
+    $row_data = mysqli_fetch_assoc($select_result);
+    $row_count = mysqli_num_rows($select_result);
+    $user_ip = getIPAddress();
+    //check if user have items |! -> redirect to payment | index 
+    $select_cart_query = "SELECT * FROM `card_details` WHERE ip_address='$user_ip'";
+    $select_cart_result = mysqli_query($con, $select_cart_query);
+    $row_cart_count = mysqli_num_rows($select_cart_result);
+    //user check about username & pass
+    if ($row_count > 0) {
+        if (password_verify($user_password, $row_data['user_password'])) {
+            // echo "<script>alert('Login Successfully')</script>";
+            $_SESSION['username'] = $user_username;
+            if ($row_count == 1 && $row_cart_count == 0) {
+                $_SESSION['username'] = $user_username;
+                echo "<script>alert('Login Successfully');</script>";
+                echo "<script>window.open('profile.php','_self');</script>";
+            } else if ($row_count == 1 && $row_cart_count > 0) {
+                $_SESSION['username'] = $user_username;
+                echo "<script>alert('Login Successfully');</script>";
+                echo "<script>window.open('payment.php','_self');</script>";
+            }
+        } else {
+            echo "<script>alert('Invalid Credentials')</script>";
+        }
+    } else {
+        echo "<script>alert('Invalid Credentials')</script>";
+    }
+}
+?>
