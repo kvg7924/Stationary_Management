e92fab28fc82eeef373d517d51fa3458709358ce
Nups1910 <nupurpatel1910.np@gmail.com>
2025-03-16 14:50:11 -0600

    Added user registration logic with validation and password hashing

--- Stationary_Management-main/Stationary_Management-main/users_area/user_registration.php
+++ Stationary_Management-main/Stationary_Management-main/users_area/user_registration.php
@@ -71,21 +71,49 @@ include('../functions/common_functions.php');
     <script src="./assets//js/bootstrap.bundle.js"></script>
 </body>
 
+</html>
+<!-- php code  -->
 <?php
 if (isset($_POST['user_register'])) {
+    $user_username = $_POST['user_username'];
+    $user_email = $_POST['user_email'];
+    $user_password = $_POST['user_password'];
+    $hash_password = password_hash($user_password,PASSWORD_DEFAULT);
+    $conf_user_password = $_POST['conf_user_password'];
+    $user_address = $_POST['user_address'];
+    $user_mobile = $_POST['user_mobile'];
+    $user_image = $_FILES['user_image']['name'];
+    $user_image_tmp = $_FILES['user_image']['tmp_name'];
     $user_ip = getIPAddress();
-
-    // Select cart items and check if user has items in the cart
-    $select_cart_items = "SELECT * FROM `card_details` WHERE ip_address='$user_ip'";
-    $select_cart_items_result = mysqli_query($con, $select_cart_items);
-    $rows_count_cart_items = mysqli_num_rows($select_cart_items_result);
-
-    if ($rows_count_cart_items > 0) {
-        $_SESSION['username'] = $user_username;
-        echo "<script>window.alert('You have items in your cart');</script>";
-        echo "<script>window.open('checkout.php','_self');</script>";
+    // check if user exist or not
+    $select_query = "SELECT * FROM `user_table` WHERE username='$user_username' OR user_email='$user_email'";
+    $select_result = mysqli_query($con, $select_query);
+    $rows_count = mysqli_num_rows($select_result);
+    if ($rows_count > 0) {
+        echo "<script>window.alert('Username | Email already exist');</script>";
+    } else if ($user_password != $conf_user_password) {
+        echo "<script>window.alert('Passwords are not match');</script>";
     } else {
-        echo "<script>window.open('../index.php','_self');</script>";
+        // insert query
+        move_uploaded_file($user_image_tmp, "./user_images/$user_image");
+        $insert_query = "INSERT INTO `user_table` (username,user_email,user_password,user_image,user_ip,user_address,user_mobile) VALUES ('$user_username','$user_email','$hash_password','$user_image','$user_ip','$user_address','$user_mobile')";
+        $insert_result = mysqli_query($con, $insert_query);
+        if ($insert_result) {
+            echo "<script>window.alert('User added successfully');</script>";
+        } else {
+            die(mysqli_error($con));
+        }
     }
+    // //select cart items check if items in cart go to checkout !| go to index.php
+    // $select_cart_items = "SELECT * FROM `card_details` WHERE ip_address='$user_ip'";
+    // $select_cart_items_result = mysqli_query($con,$select_cart_items);
+    // $rows_count_cart_items = mysqli_num_rows($select_cart_items_result);
+    // if($rows_count_cart_items > 0 ){
+    //     $_SESSION['username'] = $user_username;
+    //     echo "<script>window.alert('You have items in your cart');</script>";
+    //     echo "<script>window.open('checkout.php','_self');</script>";
+    // }else{
+    //     echo "<script>window.open('../index.php','_self');</script>";
+    // }
 }
 ?>
