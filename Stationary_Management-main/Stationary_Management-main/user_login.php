9f29a28da1faf156ccf15abaf67e658fd0d31e45
Nups1910 <nupurpatel1910.np@gmail.com>
2025-03-15 18:03:15 -0600

    Added login page UI with username and password fields

---
+++ Stationary_Management-main/Stationary_Management-main/user_login.php
@@ -0,0 +1,50 @@
+<?php
+include('../includes/connect.php');
+include('../functions/common_functions.php');
+@session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ecommerce User Login Page</title>
+    <link rel="stylesheet" href="../assets/css/bootstrap.css" />
+    <link rel="stylesheet" href="../assets/css/main.css" />
+</head>
+
+<body>
+
+    <div class="register">
+        <div class="container py-3">
+            <h2 class="text-center mb-4">User Login</h2>
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <form action="" method="post" class="d-flex flex-column gap-4">
+                        <!-- username field  -->
+                        <div class="form-outline">
+                            <label for="user_username" class="form-label">Username</label>
+                            <input type="text" placeholder="Enter your username" autocomplete="off" required="required" name="user_username" id="user_username" class="form-control">
+                        </div>
+                        <!-- password field  -->
+                        <div class="form-outline">
+                            <label for="user_password" class="form-label">Password</label>
+                            <input type="password" placeholder="Enter your password" autocomplete="off" required="required" name="user_password" id="user_password" class="form-control">
+                        </div>
+                        <div><a href="" class="text-decoration-underline">Forget your password?</a></div>
+                        <div>
+                            <input type="submit" value="Login" class="btn btn-primary mb-2" name="user_login">
+                            <p>
+                                Don't have an account? <a href="user_registration.php" class="text-primary text-decoration-underline"><strong>Register</strong></a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="./assets//js/bootstrap.bundle.js"></script>
+</body>
+
+</html>
