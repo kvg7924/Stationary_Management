98372215cf4404570bb450e5bec6b110b527698d
Nups1910 <nupurpatel1910.np@gmail.com>
2025-03-16 14:52:18 -0600

    Removed unnecessary code and changed the title name

--- Stationary_Management-main/Stationary_Management-main/users_area/user_registration.php
+++ Stationary_Management-main/Stationary_Management-main/users_area/user_registration.php
@@ -9,7 +9,7 @@ include('../functions/common_functions.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecommerce User Registeration Page</title>
+    <title>Stationary Store User Registeration Page</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.css" />
     <link rel="stylesheet" href="../assets/css/main.css" />
 </head>
@@ -22,41 +22,49 @@ include('../functions/common_functions.php');
             <div class="row justify-content-center">
                 <div class="col-lg-6">
                     <form action="" method="post" enctype="multipart/form-data" class="d-flex flex-column gap-4">
+
                         <!-- username field  -->
                         <div class="form-outline">
                             <label for="user_username" class="form-label">Username</label>
                             <input type="text" placeholder="Enter your username" autocomplete="off" required="required" name="user_username" id="user_username" class="form-control">
                         </div>
+
                         <!-- email field  -->
                         <div class="form-outline">
                             <label for="user_email" class="form-label">Email</label>
                             <input type="email" placeholder="Enter your email" autocomplete="off" required="required" name="user_email" id="user_email" class="form-control">
                         </div>
+
                         <!-- image field  -->
                         <div class="form-outline">
                             <label for="user_image" class="form-label">User Image</label>
                             <input type="file" required="required" name="user_image" id="user_image" class="form-control">
                         </div>
+
                         <!-- password field  -->
                         <div class="form-outline">
                             <label for="user_password" class="form-label">Password</label>
                             <input type="password" placeholder="Enter your password" autocomplete="off" required="required" name="user_password" id="user_password" class="form-control">
                         </div>
+
                         <!-- confirm password field  -->
                         <div class="form-outline">
                             <label for="conf_user_password" class="form-label">Confirm Password</label>
                             <input type="password" placeholder="Confirm your password" autocomplete="off" required="required" name="conf_user_password" id="conf_user_password" class="form-control">
                         </div>
+
                         <!-- address field  -->
                         <div class="form-outline">
                             <label for="user_address" class="form-label">Address</label>
                             <input type="text" placeholder="Enter your address" autocomplete="off" required="required" name="user_address" id="user_address" class="form-control">
                         </div>
+
                         <!-- mobile field  -->
                         <div class="form-outline">
                             <label for="user_mobile" class="form-label">Mobile</label>
                             <input type="text" placeholder="Enter your mobile" autocomplete="off" required="required" name="user_mobile" id="user_mobile" class="form-control">
                         </div>
+
                         <div>
                             <input type="submit" value="Register" class="btn btn-primary mb-2" name="user_register">
                             <p>
@@ -72,6 +80,7 @@ include('../functions/common_functions.php');
 </body>
 
 </html>
+
 <!-- php code  -->
 <?php
 if (isset($_POST['user_register'])) {
@@ -85,6 +94,7 @@ if (isset($_POST['user_register'])) {
     $user_image = $_FILES['user_image']['name'];
     $user_image_tmp = $_FILES['user_image']['tmp_name'];
     $user_ip = getIPAddress();
+
     // check if user exist or not
     $select_query = "SELECT * FROM `user_table` WHERE username='$user_username' OR user_email='$user_email'";
     $select_result = mysqli_query($con, $select_query);
@@ -104,16 +114,5 @@ if (isset($_POST['user_register'])) {
             die(mysqli_error($con));
         }
     }
-    // //select cart items check if items in cart go to checkout !| go to index.php
-    // $select_cart_items = "SELECT * FROM `card_details` WHERE ip_address='$user_ip'";
-    // $select_cart_items_result = mysqli_query($con,$select_cart_items);
-    // $rows_count_cart_items = mysqli_num_rows($select_cart_items_result);
-    // if($rows_count_cart_items > 0 ){
-    //     $_SESSION['username'] = $user_username;
-    //     echo "<script>window.alert('You have items in your cart');</script>";
-    //     echo "<script>window.open('checkout.php','_self');</script>";
-    // }else{
-    //     echo "<script>window.open('../index.php','_self');</script>";
-    // }
 }
 ?>
